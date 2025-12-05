ab88ee6cfef3e3ebe4a8fba016c7f5af62379e2e
Ktsarvi <alasgarovtimur@gmail.com>
2025-12-05 23:24:37 +0400

    implement projects schema in prisma and projects procedure

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -6,7 +6,7 @@ import {
   createNetwork,
   type Tool,
 } from "@inngest/agent-kit";
-import { Sandbox } from "@e2b/code-interpreter"; 
+import { Sandbox } from "@e2b/code-interpreter";
 import { getSandbox, lastAssistantTextMessageContent } from "./utils";
 import z from "zod";
 import { PROMPT } from "@/prompt";
@@ -78,7 +78,10 @@ export const codeAgentFunction = inngest.createFunction(
               })
             ),
           }),
-          handler: async ({ files }, { step, network }: Tool.Options<AgentState>) => {
+          handler: async (
+            { files },
+            { step, network }: Tool.Options<AgentState>
+          ) => {
             const newFiles = await step?.run(
               "CreateOrUpdateFiles",
               async () => {
@@ -172,6 +175,7 @@ export const codeAgentFunction = inngest.createFunction(
       if (!isError) {
         return await prisma.message.create({
           data: {
+            projectId: event.data.projectId,
             content: "Something went wrong. Please try again.",
             role: "ASSISTANT",
             type: "ERROR",
@@ -181,6 +185,7 @@ export const codeAgentFunction = inngest.createFunction(
 
       return await prisma.message.create({
         data: {
+          projectId: event.data.projectId,
           content: result.state.data.summary,
           role: "ASSISTANT",
           type: "RESULT",
@@ -188,7 +193,7 @@ export const codeAgentFunction = inngest.createFunction(
             create: {
               sandboxUrl: sandboxUrl,
               title: "Fragment",
-              files: result.state.data.files,
+              files: result.state.data.files || {}, // might delete "|| {}"
             },
           },
         },
--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -16,12 +16,17 @@ export const messagesRouter = createTRPCRouter({
   create: baseProcedure
     .input(
       z.object({
-        value: z.string().min(1, { message: "Message is required" }),
+        value: z
+          .string()
+          .min(1, { message: "Message is required" })
+          .max(10000, { message: "Message is too long" }),
+        projectId: z.string().min(1, { message: "Project ID is required" }),
       })
     )
     .mutation(async ({ input }) => {
       const createdMessage = await prisma.message.create({
         data: {
+          projectId: input.projectId,
           content: input.value,
           role: "USER",
           type: "RESULT",
@@ -30,7 +35,7 @@ export const messagesRouter = createTRPCRouter({
 
       await inngest.send({
         name: "code-agent/run",
-        data: { value: input.value },
+        data: { value: input.value, projectId: input.projectId },
       });
 
       return createdMessage;
