eeb939a0674a1dbc3aadbda5056de6a4ce082db6
Ktsarvi <alasgarovtimur@gmail.com>
2025-12-10 02:54:23 +0400

    implement ui for messages part

--- src/modules/messages/server/procedures.ts
+++ src/modules/messages/server/procedures.ts
@@ -4,22 +4,34 @@ import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import z from "zod";
 
 export const messagesRouter = createTRPCRouter({
-  getMany: baseProcedure.query(async () => {
-    const messages = await prisma.message.findMany({
-      orderBy: {
-        updatedAt: "desc",
-      },
-    });
+  getMany: baseProcedure
+    .input(
+      z.object({
+        projectId: z.string().min(1, { message: "Project ID is required" }),
+      })
+    )
+    .query(async ({ input }) => {
+      const messages = await prisma.message.findMany({
+        where: {
+          projectId: input.projectId,
+        },
+        include: {
+          fragment: true,
+        },
+        orderBy: {
+          updatedAt: "asc",
+        },
+      });
 
-    return messages;
-  }),
+      return messages;
+    }),
   create: baseProcedure
     .input(
       z.object({
         value: z
           .string()
-          .min(1, { message: "Message is required" })
-          .max(10000, { message: "Message is too long" }),
+          .min(1, { message: "Value is required" })
+          .max(10000, { message: "Value is too long" }),
         projectId: z.string().min(1, { message: "Project ID is required" }),
       })
     )
